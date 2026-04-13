65dc7a196b49012c4c00de9bb5feca0741e497d6
Aryan Yadav <caryan830@gmail.com>
2026-04-14 03:05:28 +0530

    add new models

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { loginUser, logoutUser, registerUser, refreshAccessToken, changeCurrentPassword, getcurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage, getUserChannelProfile, getwatchHistory } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middlewere.js";
-import jwt, { verify } from "jsonwebtoken";
+//import jwt, { verify } from "jsonwebtoken";
 import { varifyJWT } from "../middlewares/auth.middlewere.js";
 
 const router = Router()
@@ -29,7 +29,6 @@ router.route("/current-user").get(varifyJWT,getcurrentUser)
 router.route("/update-account").patch(varifyJWT,updateAccountDetails)
 router.route("/avatar").patch(varifyJWT,upload.single("avatar"), updateUserAvatar)
 router.route("/cover-image").patch(varifyJWT,upload.single("/CoverImage"),updateUserCoverImage)
-
 router.route("/c/:username").get(varifyJWT,getUserChannelProfile)
 router.route("/history").get(varifyJWT , getwatchHistory)  
 
