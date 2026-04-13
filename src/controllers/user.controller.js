5005590d70f67fa2e3dbefea2ee01e7c32431961
Aryan Yadav <caryan830@gmail.com>
2026-04-14 00:00:27 +0530

    add getwachistory endpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -7,6 +7,7 @@ import jwt from "jsonwebtoken"
 import { response } from "express";
 import { match } from "assert";
 import { lookup } from "dns";
+import mongoose from "mongoose";
 
 
 const generateAccessAndRefreshTokens = async (userId) => {
@@ -460,6 +461,64 @@ const getUserChannelProfile= asyncHandler(async(req, res)=>{
      )
 })
 
+const getwatchHistory= asyncHandler(async(req,res)=>{
+    const user= await User.aggregate([
+        {
+            $match:{
+                _id:new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup:{
+                from:"videos",
+                localField:"watchHistory",
+                foreignField:"_id",
+                as:"watchHistory",
+                pipeline:[
+                    {
+                        $lookup:{
+                            from:"users",
+                            localField:"owner",
+                            foreignField:"_id",
+                            as:"owner",
+                            pipeline:{
+                                $project:{
+                                    fullName: 1,
+                                    usermname:1,
+                                    avatar:1
+                                }
+                            }
+                                                
+                        }
+                    },
+                    {
+                        $addFields:{
+                            owner:{
+                                $first:"$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            user[0].watchHIstory,
+            "watch history fatch successfully"
+        )
+    )
+})
+
+
+
+
+
 export {
     registerUser,
     loginUser,
@@ -470,6 +529,6 @@ export {
     updateAccountDetails,
     updateUserAvatar,
     updateUserCoverImage,
-    getUserChannelProfile
+    getUserChannelProfile,
+    getwatchHistory
 }
-
